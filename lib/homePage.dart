e5c9d77be3c108b8707b2f00644dc27706d72c46
João Fernando <joaofernandocunha06@gmail.com>
2026-03-12 17:23:58 -0300

    added a drawer, fixed the space for the listview and added a button for adding a new register

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test01_db_interface/myWidgets.dart';
 
 class MyHomePage extends StatelessWidget{
   const MyHomePage({super.key});
@@ -11,93 +12,90 @@ class MyHomePage extends StatelessWidget{
 
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.white,
-                  Color.fromARGB(255, 128, 128, 128)
-                ]
-            ),
-          ),
-          child:
-          Stack(children: [
-            Center(
-              child: Image(
-                width: imageWidth * 2,
-                height: imageHeight / 2,
-                image: AssetImage("assets/logo.png"),
-              )
-            ),
-            Column(
-              children: [
-                MySearchBar(),
-                //SizedBox(height: 30, child: Container(color: Colors.blue,))
+        drawer: Scaffold(
+          // Esta é a tela que vai "puxar" da esquerda
+          drawer: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.3, // Ocupa 30% da tela
+            child: Drawer(
+              child: Column(
+                children: [
+                  const DrawerHeader(
+                    decoration: BoxDecoration(color: Colors.blueGrey),
+                    child: Center(child: Text("Configurações")),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.dark_mode),
+                    title: const Text("Tema Escuro"),
+                    onTap: () {},
+                  ),
                 ],
               ),
-            ],
+            ),
           ),
-        )
-      )
-    );
-  }
-}
-
-class MySearchBar extends StatelessWidget {
-  const MySearchBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsetsGeometry.only(bottom: 10),
-      child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(child:
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.white,
-                    Color.fromARGB(255, 166, 166, 166),
-                  ],
+          body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
-                ),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(9),
-                  bottomRight: Radius.circular(9),
-                ),
+                  colors: [
+                    Colors.white,
+                    Color.fromARGB(255, 128, 128, 128)
+                  ]
               ),
-              padding: EdgeInsets.only(left: 5, right: 5, bottom: 7),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(9),
-                    bottomLeft: Radius.circular(9),
-                  ),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.only(left: 80, right: 80, bottom: 20, top: 10),
-                  child: SearchBar(
-                    hintText: "Pesquisar...",
-                    backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 222, 222, 222)),
-                    shape: WidgetStatePropertyAll<OutlinedBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(9)
-                        )
+            ),
+            child:
+            Stack(children: [
+              // ------------------------ LOGO -----------------------------------
+              Center(
+                  child: Image(
+                    width: imageWidth * 2,
+                    height: imageHeight / 2,
+                    image: AssetImage("assets/logo.png"),
+                  )
+              ),
+              Column(
+                children: [
+                  // ------------------- SEARCHBAR -------------------------------
+                  MySearchBar(),
+
+                  SizedBox( // define o tamanho do espaço disponível para o listview
+                    height: screenSize.height * ((84)/100),
+                    // ----------------- LIST VIEW -------------------------------
+                    child: ListView.builder(
+                      itemCount: 20,
+                      itemBuilder: (context, index) => ProvisoryWidget(index: index),
                     ),
                   ),
-                ),
+                ],
               ),
-            )
-            )
-          ]
-      ),
+              Positioned(
+                  bottom: 30,
+                  right: 30,
+                  child: SizedBox(
+                      height: 80,
+                      width: 80,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.indigo,      // Cor do botão
+                          foregroundColor: Colors.white,     // Cor do ícone/texto
+                          fixedSize: const Size(80, 80),
+                          padding: EdgeInsets.zero,// LARGURA e ALTURA iguais para o quadrado
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero, // Remove os cantos arredondados
+                          ),
+                        ),
+                        child: const Icon(Icons.add, size: 30),
+                      )
+                  )
+                ),
+              ],
+            ),
+          )
+        ),
+      )
     );
   }
 }
