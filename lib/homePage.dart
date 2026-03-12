3732fe574296c5da4740f0fd940335d603c58c4e
João Fernando <joaofernandocunha06@gmail.com>
2026-03-12 15:30:54 -0300

    fixed the logo and put a small padding in the search bar

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -30,11 +30,14 @@ class MyHomePage extends StatelessWidget{
               child: Image(
                 width: imageWidth * 2,
                 height: imageHeight / 2,
-                image: AssetImage("logo.png"),
+                image: AssetImage("assets/logo.png"),
               )
             ),
             Column(
-              children: [MySearchBar()],
+              children: [
+                MySearchBar(),
+                //SizedBox(height: 30, child: Container(color: Colors.blue,))
+                ],
               ),
             ],
           ),
@@ -49,50 +52,52 @@ class MySearchBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(child:
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Colors.white,
-                  Color.fromARGB(255, 166, 166, 166),
-                ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(9),
-                bottomRight: Radius.circular(9),
-              ),
-            ),
-            padding: EdgeInsets.only(left: 5, right: 5, bottom: 7),
-            child: Container(
+    return Padding(padding: EdgeInsetsGeometry.only(bottom: 10),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(child:
+            Container(
               decoration: BoxDecoration(
-                color: Colors.white,
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.white,
+                    Color.fromARGB(255, 166, 166, 166),
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
                 borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(9),
                   bottomLeft: Radius.circular(9),
+                  bottomRight: Radius.circular(9),
                 ),
               ),
-              child: Padding(
-                padding: EdgeInsets.only(left: 80, right: 80, bottom: 20, top: 10),
-                child: SearchBar(
-                  hintText: "Pesquisar...",
-                  backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 222, 222, 222)),
-                  shape: WidgetStatePropertyAll<OutlinedBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(9)
-                    )
+              padding: EdgeInsets.only(left: 5, right: 5, bottom: 7),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(9),
+                    bottomLeft: Radius.circular(9),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(left: 80, right: 80, bottom: 20, top: 10),
+                  child: SearchBar(
+                    hintText: "Pesquisar...",
+                    backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 222, 222, 222)),
+                    shape: WidgetStatePropertyAll<OutlinedBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(9)
+                        )
+                    ),
                   ),
                 ),
               ),
-            ),
-          )
-        )
-      ]
+            )
+            )
+          ]
+      ),
     );
   }
 }
